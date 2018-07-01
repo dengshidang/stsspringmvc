18cd284f89108dc597466e68c84dedc39a51aeb0
dengshidang <516164386@qq.com>
2018-07-02 01:36:26 +0800

    2

--- masterspringmvc/src/main/java/masterspringmvc/MasterspringmvcApplication.java
+++ masterspringmvc/src/main/java/masterspringmvc/MasterspringmvcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MasterspringmvcApplication {
-
+  //123
 	public static void main(String[] args) {
 		SpringApplication.run(MasterspringmvcApplication.class, args);
 	}
